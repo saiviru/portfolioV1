82bedd3c7b84b62fa6e9df98fbe5ee51981fde4c
Sai Kiran <saiviru.kiran@gmail.com>
2025-12-20 01:17:50 +0530

    vercel based config

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
         ]
       : []),
   ],
+  base: process.env.VITE_BASE_PATH,
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
